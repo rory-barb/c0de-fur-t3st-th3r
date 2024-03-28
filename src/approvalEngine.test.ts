a37267023aa0852199d55bfc4d7166910e477c6e
Rory MacDonald <rory.macdonald@buildarocketboy.com>
2024-03-28 22:33:54 +0000

    add first test case for 4 hour refund policy

--- src/approvalEngine.test.ts
+++ src/approvalEngine.test.ts
@@ -1,17 +1,36 @@
-import { expect, test } from "bun:test";
+import { expect, test, describe } from "bun:test";
 import { isRefundApproved } from "./approvalEngine";
 
-test("Should return true for a customer request", () => {
-  const result = isRefundApproved({
-    name: "Emma Smith",
-    customerLocation: "US (PST)",
-    signUpDate: "1/2/2020",
-    requestSource: "phone",
-    investmentDate: "1/2/2021",
-    investmentTime: "06:00",
-    refundRequestDate: "1/2/2021",
-    refundRequestTime: "09:00",
-  });
+describe("Approved Cases - Should return true", () => {
+  test("for a customer request by phone less than 4 hours from investment time", () => {
+    const result = isRefundApproved({
+      name: "JoJo",
+      customerLocation: "US (PST)",
+      signUpDate: "1/2/2020",
+      requestSource: "phone",
+      investmentDate: "1/2/2021",
+      investmentTime: "06:00",
+      refundRequestDate: "1/2/2021",
+      refundRequestTime: "09:00",
+    });
 
-  expect(result).toBe(true);
+    expect(result).toBe(true);
+  });
+});
+
+describe("Failed Cases - Should return false", () => {
+  test("for a customer request by phone greather than 4 hours from investment time", () => {
+    const result = isRefundApproved({
+      name: "Marky Mark",
+      customerLocation: "US (PST)",
+      signUpDate: "1/2/2020",
+      requestSource: "phone",
+      investmentDate: "1/2/2021",
+      investmentTime: "06:00",
+      refundRequestDate: "1/2/2021",
+      refundRequestTime: "10:00",
+    });
+
+    expect(result).toBe(false);
+  });
 });
--- src/approvalEngine.ts
+++ src/approvalEngine.ts
@@ -1,5 +1,41 @@
 import { CustomerRequest } from "./customerRequests";
 
+const determineCustomersRefundWindow = () => {
+  return 4;
+};
+
+const determineHoursBeforeRefundRequest = (
+  investmentDate: string,
+  investmentTime: string,
+  refundRequestDate: string,
+  refundRequestTime: string
+) => {
+  const investmentTimestamp = new Date(`${investmentDate} ${investmentTime}`);
+  const refundRequestTimestamp = new Date(
+    `${refundRequestDate} ${refundRequestTime}`
+  );
+
+  return Math.abs(+investmentTimestamp - +refundRequestTimestamp) / 36e5;
+};
+
 export const isRefundApproved = (customerRequest: CustomerRequest) => {
+  const {
+    investmentDate,
+    investmentTime,
+    refundRequestDate,
+    refundRequestTime,
+  } = customerRequest;
+
+  const hoursBeforeRefundRequest = determineHoursBeforeRefundRequest(
+    investmentDate,
+    investmentTime,
+    refundRequestDate,
+    refundRequestTime
+  );
+
+  const customersRefundWindow = determineCustomersRefundWindow();
+
+  if (hoursBeforeRefundRequest >= customersRefundWindow) return false;
+
   return true;
 };
