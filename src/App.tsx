dc9627d87a16296de5f41173ea093ff6f9d20ba6
Rory MacDonald <rory.macdonald@buildarocketboy.com>
2024-03-28 21:55:07 +0000

    table build with always true approved engine

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import furtherLogo from "./assets/logo.webp";
+import { customerRequests } from "./customerRequests";
 
 function App() {
   return (
@@ -7,8 +8,40 @@ function App() {
       <div className="flex flex-col gap-10 p-4 border rounded">
         <img className="w-80 mx-auto" src={furtherLogo} alt="Vite logo" />
 
-        <div className="p-2 border">
-          <h2>Table</h2>
+        <div className="p-4 border">
+          <table className="table-auto w-full border-collapse">
+            <thead className="bg-gray-100 font-degular font-normal text-left text-lg">
+              <tr>
+                <th className="p-2">Name</th>
+                <th className="p-2">Approved</th>
+                <th>Customer Location (timezone)</th>
+                <th>Sign up date</th>
+                <th>Request Source</th>
+                <th>Investment Date</th>
+                <th>Investment Time</th>
+                <th>Refund Request Date</th>
+                <th>Refund Request Time</th>
+              </tr>
+            </thead>
+            <tbody className="font-degular font-normal text-lg">
+              {customerRequests.map((item, index) => (
+                <tr
+                  key={index}
+                  className="border-top border-b-2 border-black mb-6"
+                >
+                  <td className="p-4">{item.name}</td>
+                  <td className="p-4">{"x" ? "✅" : "❌"}</td>
+                  <td className="p-4">{item.customerLocation}</td>
+                  <td className="p-4">{item.signUpDate}</td>
+                  <td className="p-4">{item.requestSource}</td>
+                  <td className="p-4">{item.investmentDate}</td>
+                  <td className="p-4">{item.investmentTime}</td>
+                  <td className="p-4">{item.refundRequestDate}</td>
+                  <td className="p-4">{item.refundRequestTime}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
         </div>
       </div>
     </>
---
+++ src/approvalEngine.ts
@@ -0,0 +1,3 @@
+const isRefundApproved = () => {
+  return true;
+};
